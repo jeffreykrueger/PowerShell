5e450d8cb246a7ba84a7a9460f0d85893186a8d5
stillresonance <jkrueger1868@gmail.com>
2015-04-09 14:56:15 -0400

    Added Function for re-compiling WMI mof files
    Script with function for fixing common WMI issue for SCCM clients by
    re-compiling WMI mof files.

---
+++ Function Reregister WMI dll.ps1
@@ -0,0 +1,18 @@
+﻿$PC = 'SomePC Name'
+Invoke-Command -ScriptBlock ${function:Fixwmi} -ComputerName $PC
+Invoke-Command -ComputerName $pc -ScriptBlock {Start-Service -Name CcmExec}
+
+
+
+Function FixWMI (){
+    Set-Service -Name Winmgmt -StartupType Disabled
+    Stop-Service -Name Winmgmt -Force
+    $wbem =  'C:\Windows\System32\wbem'
+    Set-Service -Name Winmgmt -StartupType Automatic
+    Start-Service -Name Winmgmt
+    Start-Sleep -Seconds 10
+    $mofget = Get-ChildItem -Path $wbem -Name '*.mof'
+    Foreach ($mof in $mofget)
+    {
+    & c:\windows\system32\wbem\mofcomp.exe "$wbem\$mof"}
+    }

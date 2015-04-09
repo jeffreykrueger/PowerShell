364843a0e519e0e0b5653540025937ba7302eb9b
stillresonance <jkrueger1868@gmail.com>
2015-04-09 14:49:55 -0400

    Added new script file
    Added script file for adding computers to an AD group

---
+++ Add ADComputer to Group.ps1
@@ -0,0 +1,10 @@
+﻿$Content = Get-Content "SOME LIST HERE"
+$Group = Get-ADGroup -Identity "SOME GROUP HERE"
+$GroupDN = $Group.DistinguishedName
+foreach ($PC in $Content) 
+{
+    $Computer = Get-ADComputer -Identity $PC
+    $ComputerDN = $Computer.DistinguishedName
+    
+    Add-ADGroupMember $GroupDN -Members $ComputerDN
+}
